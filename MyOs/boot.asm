6c33c8bcd2ef6e3f1dea1d05acbc1fda57e65654
Qydro <115502577+wldoui@users.noreply.github.com>
2025-05-15 15:33:09 +0300

    Че то с ос

--- MyOs/boot.asm
+++ MyOs/boot.asm
@@ -1,63 +1,39 @@
 org 0x7C00
 
 start:
-	mov ah, 0x0e
-	mov bx, string
+    mov ax, 0x13
+	int 0x10
+    jmp draw_pixel
 
-print:
-    mov al, [bx]
-    cmp al, 0
-    je exit
-    int 0x10
-    inc bx
-    jmp print
-exit:
-    mov bx, 0
-    jmp read_key
-
-string:
-    db "sexa le wyldo", 0x0a, "lol sosi", 0x0a, "lol sosi", 0x0a, 0x0d, "kusk", 0
-string2:
-db "SYKA SYKA IDI NAHYI BLYAT DA", 0x0a, "lol troll syks", 0x0a, "nah id win", 0x0a, 0x0d, "kiosk", 0
-
-    
-read_key:
-    mov ah, 0x00
-    int 0x16
-
-    cmp al, 0xE0
-    je check_arrow
-
-    mov ah, 0x0e
+draw_pixel:
+    ;Графический режим 
+	;Размер экрана 320x200, 256 цветов
+    mov ax, 0x13
     int 0x10
 
-    jmp read_key
+    mov ax, 0xA000
+    mov es, ax
+
+    mov di, 320 * 100 + 150
+
+    mov cx, 10
+    mov al, 0x40
+cycle:
+	call render
+	jmp cycle
+render:
+	;doLoopX:
+		;doLoopY:
+			
+		;endLoopY:
+	;endLoopX:
+	mov [es:di], al
+	inc di
 	
-check_arrow:
-    mov ah, 0x00
-    int 0x16
-
-    cmp ah, 0x4B
-    je move_cursor_left
-
-    jmp read_key
-
-move_cursor_left:
-	mov ah, 0x0e
-	mov bx, string2
-	mov ah, 0x03
-    int 0x10
-    cmp dl, 0x1B
-    je end_move 
-
-    dec dl
-
-    mov ah, 0x02  
-    int 0x10 
-	jmp read_key
+	mov al, al
+	inc al
 	
-end_move:
-	jmp $
+
 
 times 510-($-$$) db 0
-dw 0XAA55
+dw 0xAA55
